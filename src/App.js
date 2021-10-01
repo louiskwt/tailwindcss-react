8ff7f96ecd61fab473fafb754dbe3a1d449cc822
Ka Wing Tsang <kawingt2@gmail.com>
2021-10-01 11:33:59 +0800

    set up TailwindCSS in react and learned the basics

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='container p-4'>
+			<TailwindCSSButton>TailWind Btn</TailwindCSSButton>
+		</div>
+	);
+}
+
+function TailwindCSSButton(props) {
+	return (
+		<button className='bg-blue-500 text-white font-medium px-4 py-2 rounded hover:bg-blue-600'>
+			{props.children}
+		</button>
+	);
 }
 
 export default App;
